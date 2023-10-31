e5e44dc06ad1cd09dc51e41623c5d49d0e753590
Hector <ramirezgamonalhector@gmail.com>
2023-10-31 18:58:51 -0500

    Frontend of WeRecycle First Commit Implementación de insertar y listar preguntas frecuentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrequenQuestions } from './models/frequenQuestions';
+import { CreaeditaFrequenquestionsComponent } from './components/frequenquestions/creaedita-frequenquestions/creaedita-frequenquestions.component';
+import { FrequenquestionsComponent } from './components/frequenquestions/frequenquestions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'frequentquestioncontroller',
+    component: FrequenquestionsComponent,
+    children: [{ path: 'nuevo', component: CreaeditaFrequenquestionsComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
