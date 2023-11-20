6466ab6026257b68c12e3ad467f2b4b51eb4f25a
Hector <ramirezgamonalhector@gmail.com>
2023-11-19 19:43:12 -0500

    preguntas frecuentes

--- src/app/services/publication.service.ts
+++ src/app/services/publication.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
   import { Observable, Subject } from 'rxjs';
   import { environment } from 'src/environments/environment';
   import { Publication } from '../models/publication';
@@ -19,60 +19,41 @@ import { quantityPublicacionByTypeDTO } from '../models/quantityPublucationByTyp
       let token = sessionStorage.getItem('token');
 
     return this.http.get<Publication[]>(this.url,{
+      headers: new HttpHeaders()
+      .set('Authorization', `Bearer ${token}`)
+      .set('Content-Type', 'application/json'),
+    });
+    }
 
-import { Subject } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Publication } from '../models/publication';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-const base_url = environment.base;
-@Injectable({
-  providedIn: 'root',
-})
-export class PublicationService {
-  private url = `${base_url}/PublicationController`;
-  private listaCambio = new Subject<Publication[]>();
-  constructor(private http: HttpClient) {}
-
-  list() {
-    let token = sessionStorage.getItem('token');
-    return this.http.get<Publication[]>(this.url, {
-
+    
+    insert(user: Publication) {
+      let token = sessionStorage.getItem('token');
+    return this.http.post(this.url, user,{
       headers: new HttpHeaders()
         .set('Authorization', `Bearer ${token}`)
         .set('Content-Type', 'application/json'),
     });
-  }
+    }
 
-  insert(user: Publication) {
-    let token = sessionStorage.getItem('token');
-    return this.http.post(this.url, user, {
-      headers: new HttpHeaders()
-        .set('Authorization', `Bearer ${token}`)
-        .set('Content-Type', 'application/json'),
-    });
-  }
+    setlist(listaNueva: Publication[]) {
+      this.listaCambio.next(listaNueva);
+    }
 
-  setlist(listaNueva: Publication[]) {
-    this.listaCambio.next(listaNueva);
-  }
+    getlist() {
+      return this.listaCambio.asObservable();
+    }
 
-  getlist() {
-    return this.listaCambio.asObservable();
-  }
-
-  delete(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
-  }
-  listId(id: number) {
-    let token = sessionStorage.getItem('token');
-    return this.http.get<Publication>(`${this.url}/${id}`, {
-      headers: new HttpHeaders()
-        .set('Authorization', `Bearer ${token}`)
-        .set('Content-Type', 'application/json'),
-    });
-  }
-
+    delete(id: number) {
+      return this.http.delete(`${this.url}/${id}`);
+    }
+    listId(id: number) {
+      let token = sessionStorage.getItem('token');
+      return this.http.get<Publication>(`${this.url}/${id}`,{
+        headers: new HttpHeaders()
+          .set('Authorization', `Bearer ${token}`)
+          .set('Content-Type', 'application/json'),
+      });
+    }
 
     update(p: Publication) {
       let token = sessionStorage.getItem('token');
@@ -87,17 +68,9 @@ export class PublicationService {
       let token = sessionStorage.getItem('token');
 
     return this.http.get<quantityPublicacionByTypeDTO[]>(`${this.url}/cantidadPorTipo`, {
-
-  update(p: Publication) {
-    let token = sessionStorage.getItem('token');
-    return this.http.put(this.url, p, {
-
       headers: new HttpHeaders()
         .set('Authorization', `Bearer ${token}`)
         .set('Content-Type', 'application/json'),
     });
-
     }
-
-  }
 }
